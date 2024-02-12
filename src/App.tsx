af10a62a001ecb70970778053d6ae0f236b0ac77
Abhinav Sorate <abhinavsorate01@gmail.com>
2024-02-12 21:21:43 +0530

    add firebase config

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
 
+import firebaseApp from "./firebase.config";
+
 function App() {
   return (
     <>

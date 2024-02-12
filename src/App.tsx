f37a6c9771d94a17eb8374b5f6458097ad12f862
Abhinav Sorate <abhinavsorate01@gmail.com>
2024-02-12 19:58:55 +0530

    add base component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import Footer from "./components/Footer/Footer";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
